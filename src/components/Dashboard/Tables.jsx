ba6b727192e4c709db489fde3a8bbf794ed514a1
Sai Chakri D <98957092+SaiChakri-d@users.noreply.github.com>
2023-06-14 15:01:19 +0530

    nqs assignment

--- src/components/Dashboard/Tables.jsx
+++ src/components/Dashboard/Tables.jsx
@@ -61,18 +61,6 @@ export default function Tables() {
     },
   ];
 
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(3);
-
-  const OnChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const OnChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
   return (
     <>
       {" "}
